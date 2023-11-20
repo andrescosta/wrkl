f07a2db539f68b1a97bc7b4da586d0fa79b766ae
Andres <andres.costa@gmail.com>
2023-11-20 11:35:52 -0500

    -a

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -8,7 +8,7 @@ import (
 	"unsafe"
 )
 
-func log(message string) {
+func Log(message string) {
 	ptr, size := stringToPtr(message)
 	_log(ptr, size)
 	runtime.KeepAlive(message) // keep message alive until ptr is no longer needed.
@@ -17,16 +17,16 @@ func log(message string) {
 //go:wasmimport env log
 func _log(ptr, size uint32)
 
-func ptrToString(ptr uint32, size uint32) string {
+func PtrToString(ptr uint32, size uint32) string {
 	return unsafe.String((*byte)(unsafe.Pointer(uintptr(ptr))), size)
 }
 
-func stringToPtr(s string) (uint32, uint32) {
+func StringToPtr(s string) (uint32, uint32) {
 	ptr := unsafe.Pointer(unsafe.StringData(s))
 	return uint32(uintptr(ptr)), uint32(len(s))
 }
 
-func stringToLeakedPtr(s string) (uint32, uint32) {
+func StringToLeakedPtr(s string) (uint32, uint32) {
 	size := C.ulong(len(s))
 	ptr := unsafe.Pointer(C.malloc(size))
 	copy(unsafe.Slice((*byte)(ptr), size), s)
